feb602f961187cefc17b865f0331d82dd650eb3d
Mr.Whitelight <57433578+Mr-Whitelight@users.noreply.github.com>
2022-11-18 17:29:38 +0800

    Update space_vector_three_phase.m

--- space_vector_three_phase.m
+++ space_vector_three_phase.m
@@ -11,88 +11,101 @@ clc
 
 % SETUP ===================================================================
 % all physical quantities in S.I. units unless stated otherwise
-                             
-% Voltage
+h=msgbox({'Author: CHAN Yun Sang, Ethan; CHAN Ho Kwan, Tony; SIU Pui Yin, Benji','','Department of Electrical Engineering, The Hong Kong Polytechnic University','','Demonstration program: Phasor and space vector'},"Copyright Information","modal");
+uiwait(h);
+prompt = {'Enter the AC line frequency (Default is 50Hz)','How many seconds do you want the simulation to run? (Default is 0.02 seconds)' };
+dlgtitle = 'AC Line frequency & Simulation Runtime';
+dims = [1 80];
+definput = {'50','0.02'};
+user_input = inputdlg(prompt,dlgtitle,dims,definput);
 A = 10;                     % Amplitude of the AC Voltage
-B= 5;                       % Amplitude of the AC Current
-T = 360;                      % Period of the AC signal
-w = 2*pi/T;                  % Angular speed of the AC phasor
-N = 400;                   % Number of frames for animation; smaller the value, the more triagular the sinewave will be
-t = linspace(0,3*T,N);              % time increment
+f_num=str2double(user_input(1,1));
+runtime_num=str2double(user_input(2,1));
+T=1/f_num;
+N = 100;                   % Number of frames for animation; smaller the value, the more triagular the sinewave will be
+dummy=1.5;
+t = linspace(0,runtime_num,100);                  % time increment
 v = A .* sin(2*pi*t/T);     % Mathmatic function of the AC Waveform
-V1 = A .* sin(2*pi*t/T+2*pi/3);  % phase shift forward 120 degree
-V2 = A .* sin(2*pi*t/T-2*pi/3);  % phase shift backward 120 degree
+V1 = A .* sin(2*pi*t/T+2*pi/3*1);  % phase shift forward 120 degree
+V2 = A .* sin(2*pi*t/T+2*pi/3*2);  % phase shift forward 240 degree
 
-
-%vP = A .* cos(2*pi*t/T);   % Use this if you want a Cosine Waveform
+Phase_Num= [0:2];
+real=cos(2*pi/3.*Phase_Num);
+imaginary=sin(2*pi/3.*Phase_Num);
 
 % GRAPHICS ================================================================
-figure(1)
 %   Setup for plot window
+figure('Name','Phasor & Vector Time Domain Plotting','NumberTitle','off')
 set(gcf,'units','normalized'); 
-set(gcf,'position',[0.1 0.1 0.6 0.30]);
+set(gcf,'position',[0.1 0.1 0.8 0.6]);
 set(gcf,'Color',[1 1 1]);
 set(gca,'FontSize',12);
 
+
 % circle
-
 p = linspace(0,2*pi,500);
-xc = 15 .* cos(p);  % Horizontal Parameter of Circle_Voltage
-yc = 15 .* sin(p);  % Vertical Parameter of Circle_Voltage
+xc = dummy*A .* cos(p);  % Horizontal Parameter of Circle_Voltage
+yc = dummy*A .* sin(p);  % Vertical Parameter of Circle_Voltage
 
 
-for a = 1 : N 
 for c = 1 : N
-    hold off
-   axis off
- %Circle Voltage
-
-   subplot(1,2,1);
+   hold off
+   %Circle Voltage
    
-   plot([0 A*sin(w*t(c))],[0 0],'lineWidth',3,'Color',[0 0 1]); % Tracking line of circle (Blue)
+   subplot(1,2,1);
 
+   plot([0 v(c)*real(1)],[0 v(c)*imaginary(1)],'lineWidth',3,'Color',[0 0 1], 'LineStyle',':'); % Tracking line of circle (Blue) degree:0
    hold on
-  
-   plot([A*sin(w*t(c)) A*sin(w*t(c))+A*sin(w*t(c)-2*pi/3)*-0.5],[0 0+A*sin(w*t(c)-2*pi/3)*sqrt(3)/2],'lineWidth',3,'Color',[0 1 0]); % Tracking line of circle (Green)
-   plot([A*sin(w*t(c))+A*sin(w*t(c)-2*pi/3)*-0.5 A*sin(w*t(c))+A*sin(w*t(c)-2*pi/3)*-0.5+A*sin(w*t(c)+2*pi/3)*-0.5],[0+A*sin(w*t(c)-2*pi/3)*sqrt(3)/2 0+A*sin(w*t(c)-2*pi/3)*sqrt(3)/2-A*sin(w*t(c)+2*pi/3)*sqrt(3)/2],'lineWidth',3,'Color',[1 1 0]); % Tracking line of circle (yellow)
-   plot([0 A*sin(w*t(c))+A*sin(w*t(c)-2*pi/3)*-0.5+A*sin(w*t(c)+2*pi/3)*-0.5],[0 0+A*sin(w*t(c)-2*pi/3)*sqrt(3)/2-A*sin(w*t(c)+2*pi/3)*sqrt(3)/2],'lineWidth',3,'Color',[0 0 0]); % Tracking line of circle (Black)
+   plot([v(c)*real(1) v(c)*real(1)+V1(c)*real(2)],[v(c)*imaginary(1) v(c)*imaginary(1)+V1(c)*imaginary(2)],'lineWidth',3,'Color',[1 1 0], 'LineStyle',':'); % Tracking line of circle (yellow) degree:120
 
-   plot(xc,yc,'m','lineWidth',1);  % Perimeter of circle
+   plot([v(c)*real(1)+V1(c)*real(2) v(c)*real(1)+V1(c)*real(2)+V2(c)*real(3)],[v(c)*imaginary(1)+V1(c)*imaginary(2) v(c)*imaginary(1)+V1(c)*imaginary(2)+V2(c)*imaginary(3)],'lineWidth',3,'Color',[0 1 0], 'LineStyle',':');% Tracking line of circle (yellow) degree:240
+   
+   plot([0 v(c)*real(1)+V1(c)*real(2)+V2(c)*real(3)],[0 v(c)*imaginary(1)+V1(c)*imaginary(2)+V2(c)*imaginary(3)],'lineWidth',2,'Color',[0 0 0]); % Tracking line of circle (Black) combined
 
-   xP = [0 0]; yP = [-A*1.5 A*1.5]; % Background vertical coordinate axis of circle
+   plot(xc,yc,'b','lineWidth',1);  % Perimeter of circle
+   title('Space Vectors in  Complex Plane')
+   xlabel('Unit Length');
+   xP = [0 0]; yP = [-A*dummy A*dummy]; % Background vertical coordinate axis of circle
    plot(xP,yP,'k','LineWidth',1);
 
-   xP = [-A*1.5 A*1.5]; yP = [0 0];  % Background horizontal coordinate axis of circle
+   xP = [-A*dummy A*dummy]; yP = [0 0];  % Background horizontal coordinate axis of circle
    plot(xP,yP,'k','LineWidth',1);
    
    hold off
-   set(gca,'FontSize',12);
+   set(gca,'FontSize',14);
+   txtTime = ['Time: ' num2str(t(c)) 's'];
+   txtFrequency = ['Frequency: ' num2str(f_num) 'Hz'];
+   txtPeriod = ['Period: ' num2str(T) 's'];
+   text(16,10,txtTime,'FontSize',12);
+   text(16,15,txtFrequency,'FontSize',12);
+   text(16,5,txtPeriod,'FontSize',12);
    axis equal
    axis on
 
   % Sin wave Voltage 
   subplot(1,2,2);
-
+   
    xP = [0 t(end)]; yP = [0 0]; %Background horizontal axis of sin wave, [start point, end point]
    plot(xP,yP,'k','LineWidth',1);
-   xlabel('x/degree')
-   ylabel('amplitude')
    hold on
-  
+   
+   xlabel('Time in seconds')
+   ylabel('Amplitude')
+   
+   title('Balanced 3-phase currents waveform')
    xP = t(1:c); yP = v(1:c); % Horizontal component of sin wave (blue trcking line)
-   plot(xP,yP,'b','LineWidth',3); 
+   plot(xP,yP,'b','LineWidth',2); 
 
-   xP = t(1:c); yP = V1(1:c); % Horizontal component of sin wave (yellow trcking line)
-   plot(xP,yP,'y','LineWidth',3); 
+   yP2 = V1(1:c); % Horizontal component of sin wave (yellow trcking line)
+   plot(xP,yP2,'y','LineWidth',2); 
 
-   xP = t(1:c); yP = V2(1:c); % Horizontal component of sin wave (green trcking line)
-   plot(xP,yP,'g','LineWidth',3); 
+   yP3 = V2(1:c); % Horizontal component of sin wave (green trcking line)
+   plot(xP,yP3,'g','LineWidth',2); 
 
    hold off
    set(gca,'FontSize',12);
-
    axis on
-   pause(0.01) % Delay 
+   pause(0.001) % Delay 
 
 end
-end
+h1=msgbox({'Simulation completed successfully! You may now close the window.'},"Simulation End","modal");
